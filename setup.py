dd73bf8111ca582376520f97e598dccf10f1851b
Florin Ratajczak <florin.ratajczak@helmholtz-muenchen.de>
2023-04-25 12:37:42 +0200

    added docs template

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
         version="0.0.2",
         author="Florin Ratajczak",
         description="Hyperbolic NNs and GNNs in torch",
-        packages=find_packages(exclude=["tests"]),
+        packages=find_packages(exclude=["tests", "docs"]),
         install_requires=["torch>=1.11", "torch-geometric>=2"]
 )
 
--- torch_hyperbolic/nn/hact.py
+++ torch_hyperbolic/nn/hact.py
@@ -10,13 +10,13 @@ class HypAct(Module):
 
     def __init__(self, act, c_in=None, c_out=None, manifold="PoincareBall"):
         super(HypAct, self).__init__()
+
+
+
         from torch_hyperbolic.nn import HyperbolicDecoder, HyperbolicEncoder
-        #self.manifold = getattr(manifolds, manifold)()
-        #self.c_in = c_in
-        #self.c_out = c_out
         self.decoder = HyperbolicDecoder(manifold=manifold, curvature=c_in) if c_in is not None else c_in
         self.act = act
-        self.encoder = HyperbolicEncoder(manifold=manifold, curvature=c_out) if c_out is not None else c_in
+        self.encoder = HyperbolicEncoder(manifold=manifold, curvature=c_out) if c_out is not None else c_out
 
     def forward(self, x):
         if self.decoder is not None:
